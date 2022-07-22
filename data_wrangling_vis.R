28c395c20cc5d4315adf33ad8539d90fc358576f
cbird808 <cbird808@gmail.com>
2022-07-22 13:31:54 -0500

    update

--- data_wrangling_vis.R
+++ data_wrangling_vis.R
@@ -11,6 +11,8 @@ require(viridis)
 theme_set(
   theme_void()
 )
+# install.packages("sf")
+library(sf)
 
 #### USER DEFINED VARIABLES ####
 
@@ -81,8 +83,8 @@ metadata %>%
   theme_classic() 
 
 metadata %>%
-  ggplot(aes(x=lat_n,
-             y=long_e,
+  ggplot(aes(y=lat_n,
+             x=long_e,
              color = habitat)) +
   geom_point(size = 5) +
   theme_classic() 
@@ -109,12 +111,61 @@ data_all %>%
 
 
 metadata %>%
-  ggplot(aes(x=lat_n,
-             y=long_e,
+  ggplot(aes(y=lat_n,
+             x=long_e,
              color = habitat)) +
   geom_point(size = 5) +
   theme_classic()
 
 world_map <- map_data("world")
+
 ggplot(world_map, aes(x = long, y = lat, group = group)) +
   geom_polygon(fill="lightgray", colour = "white")
+
+map_data("world",
+         region = "Philippines") %>%
+  ggplot(aes(x = long, 
+             y = lat,
+             group = group)) +
+  geom_polygon(fill="lightgray",
+               colour = "black") 
+
+subregion_label_data <- 
+  map_data("world",
+           region = "Philippines") %>%
+  dplyr::group_by(subregion,
+                  group) %>%
+  dplyr::summarize(long = mean(long), 
+                   lat = mean(lat))
+
+region_label_data <- 
+  map_data("world",
+           region = "Philippines") %>%
+  dplyr::group_by(region) %>%
+  dplyr::summarize(long = mean(long), 
+                   lat = mean(lat))
+
+map_data("world",
+         region = "Philippines") %>%
+  ggplot(aes(long,
+             lat,
+             group=group)) +
+  geom_polygon(fill="lightgray",
+               color = "black") +
+  # geom_text(data = subregion_label_data,
+  #           aes(label = subregion), 
+  #           size = 6, 
+  #           hjust = 0.5) +
+  geom_text(data = region_label_data,
+            aes(x = long,
+                y= lat,
+                label = region),
+            size = 10,
+            hjust = 0.5,
+            inherit.aes = FALSE) +
+  geom_point(data = metadata,
+             aes(x = long_e,
+                 y = lat_n),
+             inherit.aes = FALSE,
+             color = "red")
+
